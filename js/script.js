41a8f92466c8552789f3d07bbc553ef3eb9c0147
acid <oreoigniter@gmail.com>
2024-10-22 19:48:30 +0300

    more stuff

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ let renderData = (data) => {
                 <td><a href="#${element.date}" class="goto-link">#</a></td>
                 <td>${element.date}</td>
                 <td>` + element.tags.map((x) => `<span class="tag" onclick="document.getElementById('search').value = '#${x}'; search();">${x}</span>`).join('') + `</td>
-                <td>` + (element.link == null ? `<span onclick="alert('cтрим фактически еще не залит, но уже в списке потому-что скоро будет залит/еще заливается.');">${element.name}</span>` : `<a href="${tg_prefix}${element.link}">${element.name}</a>`) + `</td>
+                <td>` + (element.link == null ? `<span onclick="alert('cтрим фактически еще не залит, но уже в списке потому-что скоро будет залит/еще заливается.');">${element.name}</span>` : `<a href="${tg_prefix}/${element.link}">${element.name}</a>`) + `</td>
                 ` +
                 (element.categories == null ? `<td>нету информации!</td>` : `<td><ul>
                     ` +
@@ -76,10 +76,11 @@ let reverse = () => {
 }
 
 var fuse, data, order = false;
-var tg_prefix = "https://t.me/c/2016603750/";
+var tg_prefix = "https://t.me/c/2016603750";
 
 window.onload = async () => {
     data = await loadData();
+    document.getElementById("tglink").href = tg_prefix;
     data.reverse();
     fuse = new Fuse(data, {keys: ["name", "categories", "date"]});
     if (window.location.search) {
